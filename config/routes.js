afc4a617359cb75559bd2a35de2ff4924250fc97
AgnisElouise <agniselouise@gmail.com>
2019-02-23 08:24:26 -0300

    rotas do ciclo de pagamento

--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,7 @@ module.exports = function (server){
     const router = express.Router();
     server.use('/api', router);
     
-    router.route('/teste').get(function(req, res, next){
-        res.send('funcionou');
-    });
+    //rota da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
+    billingCycleService.register(router, '/billingCycles');
 }
