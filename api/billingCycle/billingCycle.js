c3a91ad77e88333345f9b4644b402e1be3f4c66f
AgnisElouise <agniselouise@gmail.com>
2019-02-27 09:12:38 -0300

    Validando os dados no update

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -1,24 +1,55 @@
 const restful = require('node-restful');
-const moongose = restful.moongose;
+const mongoose = restful.mongoose;
 
-const creditschema = new moongose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+const creditSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    value: { 
+        type: Number, 
+        min: 0, 
+        required: true 
+    }
 });
 
-const debtschema = new moongose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
-    status: { type: String, required: false, uppercase:true,  
-        enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+const debtSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    value: { 
+        type: Number, 
+        min: 0, 
+        required: [true, 'Informe o valor do débito!'] 
+    },
+    status: { 
+        type: String, 
+        required: false, 
+        uppercase:true,  
+        enum: [ 'PAGO', 'PENDENTE', 'AGENDADO' ]
+    }
 });
 
-const billingCycleSchema = new moongose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 12,  required: true }, 
-    year: { type: Number, min: 1970, max:2100, required:true },
-    credits: [creditschema],
-    debts: [debtschema]
+const billingCycleSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    month: { 
+        type: Number, 
+        min: 1, 
+        max: 12, 
+        required: true 
+    }, 
+    year: { 
+        type: Number, 
+        min: 1970, 
+        max: 2100, 
+        required:true 
+    },
+    credits: [creditSchema],
+    debts: [debtSchema]
 });
 
 module.exports = restful.model('BillingCycle', billingCycleSchema);
--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 module.exports = mongoose.connect('mongodb://agnis:agnis10@balta-shard-00-00-kwtej.azure.mongodb.net:27017,balta-shard-00-01-kwtej.azure.mongodb.net:27017,balta-shard-00-02-kwtej.azure.mongodb.net:27017/db_finance?ssl=true&replicaSet=Balta-shard-0&authSource=admin&retryWrites=true',
 );
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'.";
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'.";
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é valido para o atributo '{PATH}'.";
